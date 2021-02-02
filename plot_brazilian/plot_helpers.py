4f113465e1881d6e3a507aeb761471720c9f3c8f
Zhe Yang <starprecursor@gmail.com>
2021-02-02 22:18:02 +0100

    Add limit comparing function

--- plot_brazilian/plot_helpers.py
+++ plot_brazilian/plot_helpers.py
@@ -80,6 +80,25 @@ def atlas_label(x, y, color=1, plot_status="Internal"):
     return
 
 
+def get_plot_color(id):
+    color_dict = {
+        0: 46,
+        1: 42,
+        2: 41,
+        3: 30,
+        4: 39,
+        5: 38,
+        6: 35,
+        7: 48,
+        8: 11,
+        9: 12,
+    }
+    if id in color_dict:
+        return color_dict[id]
+    else:
+        return 1
+
+
 def parse_bool(plot_config, section, option):
     if plot_config.has_option(section, option):
         value = plot_config.get(section, option)
@@ -185,6 +204,25 @@ def process_input(plot_config):
     return input_dict
 
 
+def process_input_compare(plot_config):
+    """Checks & gets input numbers"""
+    input_dict = {}
+    x_points = parse_float_list(plot_config, "INPUT", "x_points")
+    if not x_points:
+        logging.critical("No x_points provided!")
+        exit()
+    else:
+        input_dict["x_points"] = x_points
+    input_dict["num_limits"] = len(x_points)
+    # check whether have TrexFitter input
+    logging.info("Using limits input from config...")
+    for i in range(10):
+        input_name = "limits_compare_{}".format(i)
+        process_input_member_compare(plot_config, input_name, input_dict)
+
+    return input_dict
+
+
 def process_input_member(plot_config, input_name, input_dict):
     """Check whether the quantity of x_points and processed input member is consisted"""
     if "x_points" not in input_dict:
@@ -199,6 +237,28 @@ def process_input_member(plot_config, input_name, input_dict):
     else:
         input_dict[input_name] = input_member
         input_dict["y_points_collect"].append(input_dict[input_name])
+        if len(input_member) != num_limits:
+            logging.error(
+                "Quantity of {} is not consistent with x_points, please check!".format(
+                    input_name
+                )
+            )
+            exit()
+
+
+def process_input_member_compare(plot_config, input_name, input_dict):
+    """Check whether the quantity of x_points and processed input member is consisted"""
+    if "x_points" not in input_dict:
+        logging.critical("Please process x_points first!")
+        exit()
+    else:
+        num_limits = input_dict["num_limits"]
+    input_member = parse_float_list(plot_config, "INPUT", input_name)
+    if input_member:
+        input_dict[input_name] = input_member
+        input_dict[input_name + "_name"] = parse_str(
+            plot_config, "INPUT", input_name + "_name"
+        )
         if len(input_member) != num_limits:
             logging.error(
                 "Quantity of {} is not consistent with x_points, please check!".format(
